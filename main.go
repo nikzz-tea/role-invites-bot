ff557af4fd5f420ffc19df377668c00a7f58b4af
nikzz-tea <84617815+nikzz-tea@users.noreply.github.com>
2026-01-03 18:33:33 +0300

    initial

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token := os.Getenv("TOKEN")
+
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	log.Println("Logged as " + sess.State.User.Username + "#" + sess.State.User.Discriminator)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
